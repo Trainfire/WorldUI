34ee32a1f7cb96eaae9a0e8b05163045dc8684a6
trainfire <aarongarcha@gmail.com>
2016-10-12 19:15:28 +0100

    More tweaks.

---
+++ Assets/Framework/Animation/AnimationScaler.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Framework.Animation
+{
+    class AnimationScaler : AnimationBase
+    {
+        [SerializeField] private bool _useFromValue;
+        [SerializeField] private Vector3 _from;
+        [SerializeField] private Vector3 _to;
+        
+        private TweenVector3 _tweenVec;
+
+        void Awake()
+        {
+            _tweenVec = gameObject.AddComponent<TweenVector3>();
+            _tweenVec.Duration = Duration;
+            _tweenVec.OnDone += OnTweenDone;
+            _tweenVec.OnTweenValue += OnTween;
+        }
+
+        protected override void OnPlay()
+        {
+            base.OnPlay();
+
+            _tweenVec.Curve = Curve;
+            _tweenVec.From = _useFromValue ? _from : Target.transform.localScale;
+            _tweenVec.To = _to;
+            _tweenVec.Play();
+        }
+
+        private void OnTweenDone(Tween<Vector3> obj)
+        {
+            Target.transform.localScale = obj.Value;
+            TriggerPlayComplete();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            _tweenVec.Stop();
+        }
+
+        void OnTween(Vector3 scale)
+        {
+            Target.transform.localScale = scale;
+        }
+    }
+}
